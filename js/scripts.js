f8ee6cf8ada36d776fd391d4426310f37730a55c
Alessio Vietri <info@alessiovietri.it>
2024-07-26 12:37:53 +0200

    FINITO LO STRAZIO

--- js/scripts.js
+++ js/scripts.js
@@ -83,34 +83,139 @@
     Obiettivo: voglio, a partire da un array di numeri, creare un altro array contenente solo i numeri pari
 */
 
-const allNumbers = [1, 2, 3, 4, 5];
+// const allNumbers = [1, 2, 3, 4, 5];
 
-console.log('allNumbers:', allNumbers);
+// console.log('allNumbers:', allNumbers);
 
-// const evenNumbers = [];
-// for (let i = 0; i < allNumbers.length; i++) {
-//     if (allNumbers[i] % 2 == 0) {
-//         console.log(allNumbers[i] + ' è pari');
+// // const evenNumbers = [];
+// // for (let i = 0; i < allNumbers.length; i++) {
+// //     if (allNumbers[i] % 2 == 0) {
+// //         console.log(allNumbers[i] + ' è pari');
 
-//         evenNumbers.push(allNumbers[i]);
-//     }
+// //         evenNumbers.push(allNumbers[i]);
+// //     }
+// // }
+
+// // const evenNumbers = allNumbers.filter((num) => {
+// //     if (num % 2 == 0) {
+// //         console.log(num + ' è pari');
+
+// //         return true;
+// //     }
+
+// //     return false;
+// // });
+
+// // const evenNumbers = allNumbers.filter((num) => {
+// //     return num % 2 == 0;
+// // });
+
+// const evenNumbers = allNumbers.filter((num) => num % 2 == 0);
+
+// /* Per l'obiettivo che ci siamo posti, non è detto che l'array originale e l'array calcolato abbiano sempre la stessa lunghezza */
+// console.log(evenNumbers);
+
+/* 
+
+    -------------------------------------------------------------------------------------------------------------------------
+
+    Obiettivo: voglio creare un oggetto con una serie di chiavi i cui valori sono presi da variabili
+*/
+
+// /* 
+//     firstName
+//     lastName
+// */
+// const firstName = 'Pippo';
+// const lastName = 'Paperino';
+
+// // const myObj = {
+// //     firstName: firstName,
+// //     lastName: lastName,
+// //     email: 'alessio@boolean.careers'
+// // };
+
+// const myObj = {
+//     firstName,
+//     lastName,
+//     email: 'alessio@boolean.careers'
+// };
+
+// console.log(myObj);
+
+// // const users = [];
+// // for (let i = 0; i < 3; i++) {
+// //     const firstName = prompt('Nome:');
+// //     const lastName = 'Paperino';
+
+// //     // const myObj = {
+// //     //     firstName: firstName,
+// //     //     lastName: lastName
+// //     // };
+
+// //     const myObj = {
+// //         firstName,
+// //         lastName
+// //     };
+
+// //     console.log(myObj);
+
+// //     users.push(myObj);
+// // }
+
+// // console.log(users);
+
+/* 
+
+    -------------------------------------------------------------------------------------------------------------------------
+
+    Obiettivo: dato un oggetto contenente una persona, con le chiavi di nome e cognome, creare una funzione che mi restuisce il nome completo
+*/
+
+/*
+    {
+        firstName: 'Alessio',
+        lastName: 'Vietri'
+    }
+
+    Alessio Vietri
+*/
+
+const person = personConstructor('Alessio', 'Vietri');
+
+person.sayHi();
+
+const fullName = person.getFullName();
+console.log(fullName);
+
+const personTwo = personConstructor('Alessandro', 'Lodigiani', true);
+
+const personThree = personConstructor('Michele', 'Spiller');
+
+const fullNameTwo = personTwo.getFullName();
+console.log(fullNameTwo);
+
+const fullNameThree = personThree.getFullName();
+console.log(fullNameThree);
+
+// Definisco la funzione getFullName
+// function getFullName(user) {
+//     return user.firstName + ' ' + user.lastName;
+//     // return user.firstName + ' ' + user['lastName'];
 // }
 
-// const evenNumbers = allNumbers.filter((num) => {
-//     if (num % 2 == 0) {
-//         console.log(num + ' è pari');
-
-//         return true;
-//     }
-
-//     return false;
-// });
-
-// const evenNumbers = allNumbers.filter((num) => {
-//     return num % 2 == 0;
-// });
-
-const evenNumbers = allNumbers.filter((num) => num % 2 == 0);
-
-/* Per l'obiettivo che ci siamo posti, non è detto che l'array originale e l'array calcolato abbiano sempre la stessa lunghezza */
-console.log(evenNumbers);
+function personConstructor(firstName, lastName, isMillionaire = false) {        // Costruttore
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        isMillionaire: isMillionaire,
+        sayHi() {
+            console.log('Hi!');
+        },
+        getFullName() {
+            console.log(this);
+    
+            return this.firstName + ' ' + this.lastName;
+        }
+    };
+}
